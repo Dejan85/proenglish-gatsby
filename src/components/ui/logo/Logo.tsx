3545d93a3533153d0f0dec273fd292eeb08fa2f1
Dejan85 <xypnotica@gmail.com>
2021-05-04 03:45:16 +0200

    added nav style

--- src/components/ui/logo/Logo.tsx
+++ src/components/ui/logo/Logo.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { StaticImage } from 'gatsby-plugin-image';
+import './styles.scss';
 
 const Logo = (): JSX.Element => {
-  const Wrapper = `div`;
+  const Wrapper = 'div';
   return (
-    <Wrapper>
+    <Wrapper className="logo">
       <StaticImage src="./images/logo.png" alt="logo" placeholder="blurred" />
     </Wrapper>
   );
--- src/components/ui/nav/Nav.tsx
+++ src/components/ui/nav/Nav.tsx
@@ -6,34 +6,36 @@ import { Element } from './partials/Element';
 import { NAV_CONTENT } from './constants';
 import './styles.scss';
 
-const navHandler = () => {
-  console.log('test', 'radi');
-};
-
 const Nav = (): JSX.Element => {
-  const { toggleNav, setToggleNav } = useState<boolean>(false);
+  const [toggleNav, setToggleNav] = useState<boolean>(false);
   const Wrapper = 'nav';
+
+  const navHandler = () => {
+    setToggleNav(!toggleNav);
+  };
 
   return (
     <Wrapper className="nav">
       <FiAlignJustify className="nav__responsive-btn" onClick={navHandler} />
-      <Element as="div" className="nav__wrapper">
-        <>
-          {NAV_CONTENT.map(
-            ({ name, path }): JSX.Element => (
-              <Element as="div" key={uniqid()}>
-                <Link
-                  to={path}
-                  className="nav__link"
-                  activeClassName="nav__active"
-                >
-                  {name}
-                </Link>
-              </Element>
-            ),
-          )}
-        </>
-      </Element>
+      {toggleNav && (
+        <Element as="div" className="nav__wrapper">
+          <>
+            {NAV_CONTENT.map(
+              ({ name, path }): JSX.Element => (
+                <Element as="div" key={uniqid()}>
+                  <Link
+                    to={path}
+                    className="nav__link"
+                    activeClassName="nav__active"
+                  >
+                    {name}
+                  </Link>
+                </Element>
+              ),
+            )}
+          </>
+        </Element>
+      )}
     </Wrapper>
   );
 };
