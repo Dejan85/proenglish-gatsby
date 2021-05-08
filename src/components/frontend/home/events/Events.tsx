da9f8d246ae721c0a7addc91a321d21a58464670
Dejan85 <xypnotica@gmail.com>
2021-05-08 22:32:00 +0200

    start building events page

---
+++ src/components/frontend/home/events/Events.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './styles.scss';
+import { UiRender } from './partials';
+import { useEventsDataHook } from '~/src/hooks/index';
+
+const Events = (): JSX.Element => {
+  const {
+    filteredActiveEventsDates,
+    filteredDailyEventsData,
+    generateHighlhtDates,
+    generateDailyEvents,
+  } = useEventsDataHook();
+
+  return (
+    <UiRender
+      generateHighlhtDates={generateHighlhtDates}
+      filteredActiveEventsDates={filteredActiveEventsDates}
+      generateDailyEvents={generateDailyEvents}
+      filteredDailyEventsData={filteredDailyEventsData}
+    />
+  );
+};
+
+export default Events;
