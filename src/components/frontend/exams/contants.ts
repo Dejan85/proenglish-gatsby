d3cc51ed95878423a5c77a5b008a9cb6cda1031b
Dejan85 <xypnotica@gmail.com>
2021-05-10 01:55:29 +0200

    update

--- src/components/frontend/exams/contants.ts
+++
@@ -1,1 +0,0 @@
-export const EXAMS_SCOPE = 'EXAMS_SCOPE';
--- src/components/frontend/exams/selectors.ts
+++
@@ -1,4 +0,0 @@
-import { initialState } from './slice';
-import { EXAMS_SCOPE } from './contants';
-
-export const getExamsData = (state: any) => state[EXAMS_SCOPE] || initialState;
